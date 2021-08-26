1e20560275bba2b55abe9695fbfc94e1281536bf
Clément Boesmier <cle.chec@laposte.net>
2021-08-26 20:53:12 +0200

    ajout formulaire outils

---
+++ app/formulaire_outils/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Categories, Groupe, Media, Outils
+
+admin.site.register(Categories)
+admin.site.register(Media)
+admin.site.register(Outils)
+admin.site.register(Groupe)
---
+++ app/formulaire_outils/tests.py
@@ -0,0 +1,67 @@
+from formulaire_outils.models import Categories, Groupe, Media, Outils
+import os
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+# Create your tests here.
+
+
+class FormulaireOutilsTest(TestCase):
+    def setUp(self):
+        cat = Categories.objects.create(title="Atelier")
+        cat2 = Categories.objects.create(title="Impression 3D")
+        grp = Groupe.objects.create(nom="Cle Plate")
+        image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=open(
+                './Imgtest.jpeg', 'rb').read(), content_type='image/jpeg')
+
+        med = Media.objects.create(image=image)
+
+        outil1 = Outils.objects.create(
+            type="clé plate",
+            taille="12",
+            marque="facom",
+            image=med,
+            image_cote=med,
+            nombre=4,
+            nombre_hs=0,
+            groupe=grp,
+            commentaire="ceci est un petit commentaire de test"
+        )
+        outil2 = Outils.objects.create(
+            nom="custom Nom",
+            type="clé plate",
+            taille="8",
+            marque="facom",
+            image=med,
+            image_cote=med,
+            nombre=4,
+            nombre_hs=0,
+            groupe=grp,
+            commentaire="ceci est un petit commentaire de test"
+        )
+        outil1.categories.set([cat, cat2])
+
+    def tearDown(self):
+        os.remove("static/img/outils/test_image.jpg")
+
+    def test_categorie_existe(self):
+        categories = Categories.objects.get(pk=1)
+        self.assertEqual("Atelier", str(categories))
+
+    def test_group_existe(self):
+        grp = Groupe.objects.get(pk=1)
+        self.assertEqual("Cle Plate", str(grp))
+
+    def test_media_existe(self):
+        med = Media.objects.get(pk=1)
+        self.assertEqual("static/img/outils/test_image.jpg", str(med))
+
+    def test_outils_existe(self):
+        outil = Outils.objects.get(pk=1)
+        self.assertEqual("clé plate facom 12", str(outil))
+
+    def test_outil_custom_name(self):
+        outil = Outils.objects.get(pk=2)
+        self.assertEqual("custom Nom", str(outil))
