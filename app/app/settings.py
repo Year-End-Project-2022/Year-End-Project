898a2065084324fc8af4b87328979b8380487ccd
Clément Boesmier <cle.chec@laposte.net>
2021-10-30 12:34:48 +0200

    reset deafult password

--- app/app/settings.py
+++ app/app/settings.py
@@ -93,7 +93,7 @@ if DEBUG:
         }
     }
 else:
-    PASSDB = os.environ.get('PASSDB', 0)
+    PASSDB = 'postgres'  # os.environ.get('PASSDB', 0)
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
