58a5432288fe8b884921525023604c567cc03cb4
eliemoriceau <62339484+eliemoriceau@users.noreply.github.com>
2022-06-13 15:11:11 +0200

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
+if DEBUG or sys.argv[1] == 'test':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
