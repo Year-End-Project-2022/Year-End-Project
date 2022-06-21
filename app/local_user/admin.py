6fb8906d1d478f455b50f4a8baddfc3572f7e38c
Clément Boesmier <cle.chec@laposte.net>
2022-06-21 12:33:46 +0200

    feat affichage, recherche and filter in admin user

--- app/local_user/admin.py
+++ app/local_user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import LocalUser
+
 # Register your models here.
+
+class LocalUserAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'is_staff'
+                    )
+    
+    list_filter = ('is_staff',
+                   'is_superuser',
+                   'is_active',
+                   'groups'
+                   )
+
+    search_fields = ('username',
+                     'email',
+                     'first_name',
+                     'last_name'
+                     )
+
+admin.site.register(LocalUser, LocalUserAdmin)
