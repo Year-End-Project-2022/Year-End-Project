de97435ae85a431f628c0148757c2bb94790a3b4
Clément Boesmier <cle.chec@laposte.net>
2022-06-21 16:15:42 +0200

    add affichage, filtre and search form outil

--- app/formulaire_outils/admin.py
+++ app/formulaire_outils/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 from .models import Categories, Groupe, Media, Outils
 
+
+class OutilsAdmin(admin.ModelAdmin):
+    list_display = ('nom_func',
+                    'nombre',
+                    'nombre_hs',
+                    'groupe'
+                    )
+
+    list_filter = ('categories',
+                   'groupe',
+                   'taille',
+                   'marque',
+                   )
+
+    search_fields = ('nom',
+                     'type',
+                     'taille',
+                     'categories__title',
+                     'nombre',
+                     'nombre_hs',
+                     'groupe__nom',
+                     'commentaire',
+                     )
+
+
+admin.site.register(Outils, OutilsAdmin)
 admin.site.register(Categories)
 admin.site.register(Media)
-admin.site.register(Outils)
 admin.site.register(Groupe)
