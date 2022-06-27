d54e1b9663da1fb458d6bbf192bc576b2a4c6faf
Clément Boesmier <cle.chec@laposte.net>
2022-06-27 11:26:43 +0200

    suite tamplating login et fix error inscription

--- app/app/settings.py
+++ app/app/settings.py
@@ -150,8 +150,7 @@ AUTHENTICATION_BACKENDS = [
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-            """django.contrib.auth.password_validation
-            .UserAttributeSimilarityValidator""",
+            """django.contrib.auth.password_validation.UserAttributeSimilarityValidator""",
     },
     {
         'NAME':
