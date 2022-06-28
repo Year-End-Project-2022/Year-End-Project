a5ceff7e06b6b51f055ee7e4322c02c442ec1f97
eliemoriceau <elie.moriceau@epitech.eu>
2022-06-28 16:17:02 +0200

    save

--- app/local_user/models.py
+++ app/local_user/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 
 class LocalUser(AbstractUser):
     pass
+
+class Competance(models.Model):
+    name = models.CharField(max_length=50, primary_key=True, unique=True)
+    value = models.IntegerField()
