e1dcf94f3a2617ee5dbc33f1594a599c693e2172
Ewen pinson <ewen.pinson@epitech.eu>
2022-08-23 16:50:05 +0200

    clean form

--- app/formulaire_outils/form.py
+++ app/formulaire_outils/form.py
@@ -1,4 +1,3 @@
-from allauth.account.forms import SignupForm
 from django import forms
 from local_user.models import LocalUser, Competence
 from formulaire_outils.models import OutilsCnc
@@ -13,8 +12,8 @@ class OutilsCncCreateForm():
         outils_cnc = super(OutilsCncCreateForm, self).save(request)
         outils_cnc.user = self.cleaned_data['user']
         outils_cnc.outils_cnc = self.cleaned_data['outil_cnc']
-        outils_cnc.duration = self.cleaned_data['duration']
         outils_cnc.date = self.cleaned_data['date']
+        outils_cnc.duration = self.cleaned_data['duration']
         outils_cnc.save()
         return outils_cnc
 
@@ -27,7 +26,7 @@ class OutilsCncEditForm(forms.ModelForm):
         fields = ['date', 'duration']
     
     def save(self, request):
-        outils_cnc.duration = self.cleaned_data['duration']
         outils_cnc.date = self.cleaned_data['date']
+        outils_cnc.duration = self.cleaned_data['duration']
         outils_cnc.save()
         return outils_cnc
